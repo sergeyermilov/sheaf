ef2d6c9be78f925cf80c561dd0db05f8b4b99517
petr.pogorelov <petr.pogorelov@vk.team>
2024-07-02 16:48:41 +0300

    change

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,6 +4,7 @@ import click
 import json
 import pickle
 import pathlib
+import itertools
 
 import numpy as np
 import pandas as pd
@@ -27,15 +28,19 @@ MODELS = {
     "SheafGCN": SheafGCN,
 }
 
-pd.set_option('display.max_columns', None)
-tqdm.pandas()
+
+def as_numpy(torch_tensor):
+    if torch_tensor.device == "cpu":
+        return torch_tensor.numpy()
+
+    return torch_tensor.cpu().numpy()
 
 
 def evaluate(user_idx, final_user_Embed, final_item_Embed, interacted, k):
     user_emb = final_user_Embed[user_idx]
     scores = torch.matmul(user_emb, torch.transpose(final_item_Embed, 0, 1))
     scores[interacted] = 0.0
-    return scores.argsort(descending=True).numpy()[:k]
+    return as_numpy(scores.argsort(descending=True))[:k]
 
 
 def dcg_at_k(score, k=None):
@@ -54,16 +59,26 @@ def ndcg_at_k(score, k=None):
     ndcg = actual_dcg / best_dcg
     return ndcg
 
+
 def get_metrics(_df, k, user_embeddings, item_embeddings):
-    _df[f"reco_{k}"] = _df.progress_apply(lambda x: evaluate(x["user_id_idx"], user_embeddings, item_embeddings, x["interacted_id_idx"], k), axis=1)
-    _df[f"intersected_{k}"] = _df.apply(lambda x: list(set(x[f"reco_{k}"]).intersection(x["item_id_idx"])), axis=1)
+    reco_k = f"reco_{k}"
+    intersected_k = f"intersected_{k}"
+    recall_k = f"recall_{k}"
+    precision_k = f"precision_{k}"
+    ranks_k = f'ranks_{k}'
+    ndcg_k = f"ndcg_{k}"
 
-    _df[f"recall_{k}"] = _df.apply(lambda x: len(x[f"intersected_{k}"]) / len(x["item_id_idx"]), axis=1)
-    _df[f"precision_{k}"] = _df.apply(lambda x: len(x[f"intersected_{k}"]) / k, axis=1)
+    _df[reco_k] = _df.progress_apply(
+        lambda x: evaluate(x["user_id_idx"], user_embeddings, item_embeddings, x["interacted_id_idx"], k), axis=1)
+    _df[intersected_k] = _df.apply(lambda x: list(set(x[f"reco_{k}"]).intersection(x["item_id_idx"])), axis=1)
 
-    _df[f'ranks_{k}'] = _df.apply(lambda x: [int(movie_id in x[f"reco_{k}"]) for movie_id in x["item_id_idx"]], axis=1)
-    _df[f"ndcg_{k}"] = _df.apply(lambda x: ndcg_at_k(x[f'ranks_{k}'], k), axis=1)
-    return _df
+    _df[recall_k] = _df.apply(lambda x: len(x[f"intersected_{k}"]) / len(x["item_id_idx"]), axis=1)
+    _df[precision_k] = _df.apply(lambda x: len(x[f"intersected_{k}"]) / k, axis=1)
+
+    _df[ranks_k] = _df.apply(lambda x: [int(movie_id in x[f"reco_{k}"]) for movie_id in x["item_id_idx"]], axis=1)
+    _df[ndcg_k] = _df.apply(lambda x: ndcg_at_k(x[f'ranks_{k}'], k), axis=1).fillna(0.0)
+
+    return _df, [recall_k, precision_k, ndcg_k]
 
 
 @click.command()
@@ -84,7 +99,11 @@ def main(model, dataset, epochs, artifact_dir, report_dir, device):
     print("-----------------------------------------------")
 
     if os.getenv("CUDA_VISIBLE_DEVICE"):
-        raise Exception("You need to fix CUDA_VISIBLE_DEVICE to desired device. Distributed training is not yet supported.")
+        raise Exception(
+            "You need to fix CUDA_VISIBLE_DEVICE to desired device. Distributed training is not yet supported.")
+
+    pd.set_option('display.max_columns', None)
+    tqdm.pandas()
 
     torch.set_default_device(device)
 
@@ -96,30 +115,32 @@ def main(model, dataset, epochs, artifact_dir, report_dir, device):
     with open(str(artifact_dir.joinpath(f"DATA_{model}_{dataset}_{epochs}.pickle")), 'rb') as handle:
         ml_data_module = pickle.load(handle)
 
-    ml_1m_train = ml_data_module.train_dataset
-    ml_1m_test = ml_data_module.test_dataset
+    train_dataset = ml_data_module.train_dataset
+    test_dataset = ml_data_module.test_dataset
 
-    model = MODELS[model].load_from_checkpoint(str(artifact_dir.joinpath(f"MODEL_{model}_{dataset}_{epochs}.pickle")), dataset=ml_1m_train, latent_dim=40)
-    model.eval()
+    model_instance = MODELS[model].load_from_checkpoint(
+        str(artifact_dir.joinpath(f"MODEL_{model}_{dataset}_{epochs}.pickle")), dataset=train_dataset, latent_dim=40)
+    model_instance.eval()
+    model_instance = model_instance.to(device)
 
     with torch.no_grad():
-        _, embeddings = model(ml_1m_train.adjacency_matrix)
-        user_embeddings, item_embeddings = torch.split(embeddings, (ml_1m_train.num_users, ml_1m_train.num_items))
+        _, embeddings = model_instance(train_dataset.adjacency_matrix.to(device))
+        user_embeddings, item_embeddings = torch.split(embeddings, (train_dataset.num_users, train_dataset.num_items))
 
-    res = ml_1m_test.interacted_items_by_user_idx.copy(deep=True)
-    interactions = ml_1m_train.interacted_items_by_user_idx.copy(deep=True).rename(
+    res = test_dataset.interacted_items_by_user_idx.copy(deep=True)
+    interactions = train_dataset.interacted_items_by_user_idx.copy(deep=True).rename(
         columns={"item_id_idx": "interacted_id_idx"})
     res = res.merge(interactions, on=["user_id_idx"])
 
-    res = get_metrics(res, 5, user_embeddings, item_embeddings)
-    res = get_metrics(res, 10, user_embeddings, item_embeddings)
-    res = get_metrics(res, 20, user_embeddings, item_embeddings)
-    res = get_metrics(res, 50, user_embeddings, item_embeddings)
+    res, metrics_5 = get_metrics(res, 5, user_embeddings, item_embeddings)
+    res, metrics_10 = get_metrics(res, 10, user_embeddings, item_embeddings)
+    res, metrics_20 = get_metrics(res, 20, user_embeddings, item_embeddings)
+    res, metrics_50 = get_metrics(res, 50, user_embeddings, item_embeddings)
 
     res.to_csv(str(report_dir.joinpath(f"DETAILED_{model}_{dataset}_{epochs}.csv")))
 
     brief = dict()
-    for c in res.columns:
+    for c in itertools.chain(metrics_5, metrics_10, metrics_20, metrics_50):
         brief[c] = res[c].mean()
 
     with open(str(report_dir.joinpath(f"BRIEF_{model}_{dataset}_{epochs}.json")), "w") as brief_file:
