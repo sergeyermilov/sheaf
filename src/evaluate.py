178b8bd8a75cb68b40a5c4be17eaeb6a2ec74009
petr.pogorelov <petr.pogorelov@vk.team>
2024-07-02 15:57:46 +0300

    change

--- src/evaluate.py
+++ src/evaluate.py
@@ -66,17 +66,27 @@ def get_metrics(_df, k, user_embeddings, item_embeddings):
 @click.command()
 @click.option("--model", default="LightGCN", type=str)
 @click.option("--dataset", default="LightGCN", type=str)
-def main(model, dataset):
+@click.option("--epochs", default=5, type=int)
+@click.option("--artifact_dir", default="artifact/", type=str)
+def main(model, dataset, epochs, artifact_dir):
+    print("-----------------------------------------------")
+    print("Running model with the following configuration:")
+    print(f"model = {model}")
+    print(f"dataset = {dataset}")
+    print(f"epochs = {epochs}")
+    print(f"artifact_dir = {artifact_dir}")
+    print("-----------------------------------------------")
+
     if os.getenv("CUDA_VISIBLE_DEVICE"):
         raise Exception("You need to fix CUDA_VISIBLE_DEVICE to desired device. Distributed training is not yet supported.")
 
-    with open(f"DATA_{model}_{dataset}.pickle", 'rb') as handle:
+    with open(f"DATA_{model}_{dataset}_{epochs}.pickle", 'rb') as handle:
         ml_data_module = pickle.load(handle)
 
     ml_1m_train = ml_data_module.train_dataset
     ml_1m_test = ml_data_module.test_dataset
 
-    model = MODELS[model].load_from_checkpoint(f"MODEL_{model}_{dataset}.pickle", dataset=ml_1m_train, latent_dim=40)
+    model = MODELS[model].load_from_checkpoint(f"MODEL_{model}_{dataset}_{epochs}.pickle", dataset=ml_1m_train, latent_dim=40)
     model.eval()
 
     with torch.no_grad():
@@ -93,15 +103,15 @@ def main(model, dataset):
     res = get_metrics(res, 20, user_embeddings, item_embeddings)
     res = get_metrics(res, 50, user_embeddings, item_embeddings)
 
-    res.to_csv(f"DETAILED_{model}_{dataset}.csv")
+    res.to_csv(f"DETAILED_{model}_{dataset}_{epochs}.csv")
 
     brief = dict()
     for c in res.columns:
         brief[c] = res[c].mean()
 
-    with open(f"BRIEF_{model}_{dataset}.json", "w") as brief_file:
+    with open(f"BRIEF_{model}_{dataset}_{epochs}.json", "w") as brief_file:
         json.dump(brief, brief_file)
 
-    print(f"Evaluation results for model {model} over dataset {dataset}:")
+    print(f"Evaluation results for model {model} over dataset {dataset} that was trained on {epochs} epochs:")
     for k, v in brief.items():
         print(f"{k}: {v}")
