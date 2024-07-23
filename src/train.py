006fbc2ccbfce889afa303dce32858bc453e5456
petr.pogorelov <petr.pogorelov@vk.team>
2024-07-23 19:54:36 +0300

    All sheaf gcn algorithms are united

--- src/train.py
+++ src/train.py
@@ -1,32 +1,30 @@
 import os
+import json
 import click
 import torch
-import pickle
 import pathlib
 
 from pytorch_lightning import Trainer
+
+from src.utils import create_from_json_string
+from src.utils import serialize_dataset
+from src.utils import compute_artifact_id
 
 from src.datasets.facebook import FacebookDataModule, FACEBOOK_DATASET_RELATIVE_PATH
 from src.datasets.movie_lens_1m import MovieLensDataModule, MOVIE_LENS_DATASET_RELATIVE_PATH
 from src.datasets.yahoo_movies import YahooMoviesDataModule, YAHOO_DATASET_RELATIVE_PATH
 
-from src.models.sheaf.XSheafGCN import XSheafGCN
-from src.models.sheaf.ESheafGCN import ESheafGCN
-from src.models.sheaf.GSheafGCN import GSheafGCN
-from src.models.sheaf.GXSheafGCN import GXSheafGCN
-from src.models.sheaf.GEXSheafGCN import GEXSheafGCN
+from src.models.sheaf.ExtendableSheafGCN import ExtendableSheafGCN
 from src.models.sheaf.SheafGCN import SheafGCN
+from src.models.sheaf.ESheafGCN import ESheafGCN
 
-from src.models.LightGCN import LightGCN
-from src.models.GAT import GAT
+from src.models.graph.LightGCN import LightGCN
+from src.models.graph.GAT import GAT
 
 MODELS = {
     # sheaf models
-    "GEXSheafGCN": GEXSheafGCN,
+    "ExtendableSheafGCN": ExtendableSheafGCN,
     "ESheafGCN": ESheafGCN,
-    "XSheafGCN": XSheafGCN,
-    "GXSheafGCN": GXSheafGCN,
-    "GSheafGCN": GSheafGCN,
     "SheafGCN": SheafGCN,
     # graph models
     "LightGCN": LightGCN,
@@ -40,53 +38,68 @@ DATASETS = {
 }
 
 
-def serialize_dataset(filename, datamodule):
-    with open(filename, 'wb') as handle:
-        pickle.dump(datamodule, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
 @click.command()
 @click.option("--model", default="LightGCN", type=str)
 @click.option("--dataset", default="FACEBOOK", type=str)
-@click.option("--latent_dim", default=40, type=int)
-@click.option("--dataset_dir", default="data/", type=str)
+@click.option("--params", "-p", type=str)
+@click.option("--dataset_dir", default="data/", type=pathlib.Path)
 @click.option("--batch_size", default=1024, type=int)
 @click.option("--epochs", default=20, type=int)
 @click.option("--device", default="cuda", type=str)
-@click.option("--artifact_dir", default="artifact/", type=str)
-def main(model, dataset, latent_dim, dataset_dir, batch_size, epochs, device, artifact_dir):
+@click.option("--artifact_dir", default="artifact/", type=pathlib.Path)
+def main(model, dataset, params, dataset_dir, batch_size, epochs, device, artifact_dir):
+    artifact_params = dict(
+        model=model,
+        dataset=dataset,
+        epochs=epochs,
+        batch_size=batch_size,
+        params=params
+    )
+
+    artifact_id = compute_artifact_id(length=12, **artifact_params)
+
     print("-----------------------------------------------")
     print("Running model with the following configuration:")
+    print("-----------------------------------------------")
     print(f"model = {model}")
     print(f"dataset = {dataset}")
-    print(f"latent_dim = {latent_dim}")
+    print(f"params = {params}")
     print(f"dataset_dir = {dataset_dir}")
     print(f"batch_size = {batch_size}")
     print(f"epochs = {epochs}")
     print(f"device = {device}")
+    print(f"artifact_dir = {artifact_dir}")
+    print(f"artifact_id = {artifact_id}")
     print("-----------------------------------------------")
-    artifact_dir = pathlib.Path(artifact_dir)
+
+    artifact_dir = artifact_dir.joinpath(artifact_id)
     os.makedirs(artifact_dir, exist_ok=True)
 
+    with open(artifact_dir.joinpath("config.json"), "w") as fhandle:
+        json.dump(artifact_params, fhandle)
+
     if os.getenv("CUDA_VISIBLE_DEVICE"):
-        raise Exception("You need to fix CUDA_VISIBLE_DEVICE to desired device. Distributed training is not yet supported.")
+        raise Exception(
+            "You need to fix CUDA_VISIBLE_DEVICE to desired device. Distributed training is not yet supported.")
 
     torch.set_default_device(device)
 
     model_class = MODELS[model]
     dataset_class, dataset_path = DATASETS[dataset]
 
-    dataset_path = str(pathlib.Path(dataset_dir).joinpath(dataset_path))
+    dataset_path = str(dataset_dir.joinpath(dataset_path))
     ml_data_module = dataset_class(dataset_path, batch_size=batch_size)
     ml_data_module.setup()
 
-    serialize_dataset(str(artifact_dir.joinpath(f"DATA_{model}_{dataset}_{epochs}.pickle")), ml_data_module)
+    serialize_dataset(artifact_dir.joinpath(f"data.pickle"), ml_data_module)
 
     train_dataloader = ml_data_module.train_dataloader()
-    model_instance = model_class(latent_dim=latent_dim, dataset=ml_data_module.train_dataset)
+
+    model_class_partial = create_from_json_string(model_class, params)
+    model_instance = model_class_partial(dataset=ml_data_module.train_dataset)
     trainer = Trainer(max_epochs=epochs, log_every_n_steps=1)
     trainer.fit(model_instance, train_dataloader)
-    trainer.save_checkpoint(str(artifact_dir.joinpath(f"MODEL_{model}_{dataset}_{epochs}.pickle")))
+    trainer.save_checkpoint(str(artifact_dir.joinpath(f"model.pickle")))
 
 
 if __name__ == "__main__":
