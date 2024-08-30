090dece679b108971c15cea6578210aeadfb02c6
petr.pogorelov <petr.pogorelov@vk.team>
2024-08-30 14:55:45 +0300

    change

--- src/models/graph_utils.py
+++ src/models/graph_utils.py
@@ -5,79 +5,10 @@ from torch import Tensor
 from torch_geometric.utils.num_nodes import maybe_num_nodes
 
 
-def k_hop_subgraph_dropout(
-        node_idx: Union[int, List[int], Tensor],
-        num_hops: int,
-        edge_index: Tensor,
-        relabel_nodes: bool = False,
-        num_nodes: Optional[int] = None,
-        flow: str = 'source_to_target',
-        directed: bool = False,
-        hop_dropouts: Optional[Union[List[float], float]] = None,
-        rng: Optional[torch.Generator] = None,
-) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-    num_nodes = maybe_num_nodes(edge_index, num_nodes)
-
-    assert flow in ['source_to_target', 'target_to_source']
-    if flow == 'target_to_source':
-        row, col = edge_index
-    else:
-        col, row = edge_index
-
-    node_mask = row.new_empty(num_nodes, dtype=torch.bool)
-    edge_mask = row.new_empty(row.size(0), dtype=torch.bool)
-
-    if isinstance(node_idx, int):
-        node_idx = torch.tensor([node_idx], device=row.device)
-    elif isinstance(node_idx, (list, tuple)):
-        node_idx = torch.tensor(node_idx, device=row.device)
-    else:
-        node_idx = node_idx.to(row.device)
-
-    if isinstance(hop_dropouts, float):
-        hop_dropouts = [hop_dropouts] * num_hops
-    elif hop_dropouts is None:
-        hop_dropouts = [0] * num_hops
-
-    subsets = [node_idx]
-
-    for hop in range(num_hops):
-        node_mask.fill_(False)
-        node_mask[subsets[-1]] = True
-        torch.index_select(node_mask, 0, row, out=edge_mask)
-
-        dropout_proba = torch.zeros_like(edge_mask[edge_mask], dtype=torch.float16)
-        dropout_proba[...] = hop_dropouts[hop]
-        droped_edges = torch.bernoulli(dropout_proba, generator=rng).type(torch.bool)
-
-        edge_mask[edge_mask.clone()] = ~droped_edges
-
-        subsets.append(col[edge_mask])
-
-    subset, inv = torch.cat(subsets).unique(return_inverse=True)
-    inv = inv[:node_idx.numel()]
-
-    node_mask.fill_(False)
-    node_mask[subset] = True
-
-    if not directed:
-        edge_mask = node_mask[row] & node_mask[col]
-
-    edge_index = edge_index[:, edge_mask]
-
-    if relabel_nodes:
-        mapping = row.new_full((num_nodes,), -1)
-        mapping[subset] = torch.arange(subset.size(0), device=row.device)
-        edge_index = mapping[edge_index]
-
-    return subset, edge_index, inv, edge_mask
-
-
 def k_hop_subgraph_limit(
-        node_idx: Union[int, List[int], Tensor],
+        node_idx: Union[int, List[int], Tensor, Tensor],
         num_hops: int,
         edge_index: Tensor,
-        relabel_nodes: bool = False,
         num_nodes: Optional[int] = None,
         flow: str = 'source_to_target',
         directed: bool = False,
@@ -94,6 +25,7 @@ def k_hop_subgraph_limit(
 
     node_mask = row.new_empty(num_nodes, dtype=torch.bool)
     edge_mask = row.new_empty(row.size(0), dtype=torch.bool)
+    edge_mask_subsets = row.new_empty(row.size(0), dtype=torch.bool)
 
     if isinstance(node_idx, int):
         node_idx = torch.tensor([node_idx], device=row.device)
@@ -119,6 +51,7 @@ def k_hop_subgraph_limit(
             edge_mask[mask_ix[rand_ix]] = True
 
         subsets.append(col[edge_mask])
+        edge_mask_subsets = edge_mask_subsets | edge_mask
 
     subset, inv = torch.cat(subsets).unique(return_inverse=True)
     inv = inv[:node_idx.numel()]
@@ -127,13 +60,8 @@ def k_hop_subgraph_limit(
     node_mask[subset] = True
 
     if not directed:
-        edge_mask = node_mask[row] & node_mask[col]
+        edge_mask_subsets = node_mask[row] & node_mask[col]
 
-    edge_index = edge_index[:, edge_mask]
+    edge_index_all_hops = edge_index[:, edge_mask_subsets]
 
-    if relabel_nodes:
-        mapping = row.new_full((num_nodes,), -1)
-        mapping[subset] = torch.arange(subset.size(0), device=row.device)
-        edge_index = mapping[edge_index]
-
-    return subset, edge_index, inv, edge_mask
+    return subset, edge_index_all_hops, inv, edge_mask_subsets
