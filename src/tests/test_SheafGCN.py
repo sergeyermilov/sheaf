14287926fa714981f86bcb41e6645fd5d2bfb2c4
petr.pogorelov <petr.pogorelov@vk.team>
2024-09-28 22:55:19 +0300

    fix AT?

--- src/tests/test_SheafGCN.py
+++ src/tests/test_SheafGCN.py
@@ -16,7 +16,7 @@ class TestSheafConvLayer(TestCase):
         edge_index = torch.tensor([[0, 1, 1, 2, 2, 3],
                                    [1, 0, 2, 1, 3, 2]])
         conv = SheafConvLayer(4, edge_index)
-        print(conv.compute_left_right_map_index())
+        print(conv.compute_left_right_map_index(edge_index))
 
     def test_predict_restriction_maps(self):
         x = torch.FloatTensor([[1.0, 0.5, 0.0],
@@ -28,5 +28,5 @@ class TestSheafConvLayer(TestCase):
                                    [1, 0, 2, 1, 3, 2]])
 
         conv = SheafConvLayer(4, edge_index)
-        maps = conv.predict_restriction_maps(x)
-        print(conv.build_laplacian(maps))
+        maps = conv.predict_restriction_maps(x, edge_index)
+        print(conv.build_laplacian(x, maps, edge_index))
