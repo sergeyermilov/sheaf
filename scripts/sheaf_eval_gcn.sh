688b91b7ad124341412e27a93ee89b2b25b6968b
petr.pogorelov <petr.pogorelov@vk.team>
2024-09-30 17:28:46 +0500

    change

--- scripts/sheaf_eval_gcn.sh
+++ scripts/sheaf_eval_gcn.sh
@@ -5,10 +5,11 @@ DATASET=$2
 EPOCHS=$3
 LATENT_DIMS=$4
 BATCH_SIZE=$5
-SAMPLES=$6
+DEPTH=$6
+SAMPLES=$7
 
-NUM_HOPS=$7 # 2
-HOP_MAX_EDGES=$8 #[$BATCH_SIZE,$BATCH_SIZE*4,$BATCH_SIZE*8]
+NUM_HOPS=$8 # 2
+HOP_MAX_EDGES=$9 #[$BATCH_SIZE,$BATCH_SIZE*4,$BATCH_SIZE*8]
 
 ENABLE_SUBSAMPLING_CMD=""
 
@@ -23,14 +24,14 @@ echo "BATCH_SIZE = $BATCH_SIZE"
 ARTIFACT_DIR="./SHEAF_${DATASET}_${EPOCHS}_$(date +%s)"
 
 #LAYER_TYPES=("['hetero_global']" "['homo_global']" "['homo_simple_ffn']" "['hetero_simple_ffn']" "['hetero_global','hetero_simple_ffn']" "['homo_global','homo_simple_ffn']")
-LAYER_TYPES=("['hetero_global']" "['hetero_simple_ffn']" "['hetero_global','hetero_simple_ffn']")
+LAYER_TYPES=("['hetero_simple_ffn']" "['hetero_global']" "['hetero_global','hetero_simple_ffn']")
 
 for SEED in $(seq 1 $SAMPLES); do
   for LAYER in "${LAYER_TYPES[@]}"; do
     python -m src.train \
       --model ExtendableSheafGCN \
       --dataset-params "{'random_state':$SEED, 'batch_size':$BATCH_SIZE $ENABLE_SUBSAMPLING_CMD}" \
-      --model-params "{'latent_dim':$LATENT_DIMS,'layer_types':$LAYER,'epochs_per_operator':20}" \
+      --model-params "{'latent_dim':$LATENT_DIMS,'operator_ffn_depth':$DEPTH,'layer_types':$LAYER,'epochs_per_operator':20}" \
       --dataset $DATASET \
       --device $DEVICE \
       --epochs $EPOCHS \
